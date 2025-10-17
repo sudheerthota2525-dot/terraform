d91da3423870c0c1ff935d4027c8d226a9fb3080
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-10-17 13:37:02 -0400

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -16,9 +16,31 @@ resource "aws_instance" "terraform" {
       command = "echo Instance is destroyed"
       when    = destroy
     }
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx"
+      ]
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop nginx",
+        "echo 'successfully stopped nginx server' "
+      ]
+      when = destroy
+    }
 }
 
-resource "aws_security_group" "allow_all_tf" {
+resource "aws_security_group" "allow_all_tf"{
   name   = "allow-all-tf"
 
   egress {
