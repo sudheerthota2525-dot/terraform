b5067154ca09e3cc4e68fd56bfc23f716400fdea
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-11-17 08:45:58 -0500

    terraform

---
+++ multi-account/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "og-remote-state"
+    key    = "multi-account"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "dev"
+  profile = "dev"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "prod"
+  profile = "prod"
+}
---
+++ multi-account/variables.tf
@@ -0,0 +1,9 @@
+variable  "dev" {
+  type        = string
+  default = "457861824136"
+}
+
+variable  "prod" {
+  type        = string
+  default = "160885265516"
+}
